f4458e57693061a794eb7bd6a59f3eb265d74200
dincrash <dincrash@yandex.ru>
2022-02-04 17:14:33 +0300

    2 task 5

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,14 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+import os
+mylist = list()
+text = ""
+for filename in os.listdir("files"):
+   with open(os.path.join("files", filename), 'r') as f: # open in readonly mode
+       text = text + (f'{f.read()} ')
+       mylist.append(str(text))
+print(text)
+with open('result.txt', 'w', encoding='CP1252') as f:
+    fields = text.split(' ')
+    f.write(','.join(fields))
