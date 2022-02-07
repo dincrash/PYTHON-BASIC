fe12524997360631797734ce4ee7a91b4b14443c
dincrash <dincrash@yandex.ru>
2022-02-07 15:12:56 +0300

    3 testing 2

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -14,13 +14,15 @@ Example:
     result.txt(content: "23, 78, 3")
 """
 import os
-mylist = list()
-text = ""
-for filename in os.listdir("files"):
-   with open(os.path.join("files", filename), 'r') as f: # open in readonly mode
-       text = text + (f'{f.read()} ')
-       mylist.append(str(text))
-print(text)
-with open('result.txt', 'w', encoding='CP1252') as f:
-    fields = text.split(' ')
-    f.write(','.join(fields))
+
+def read_write(tmp_path):
+    mylist = list()
+    text = ""
+    for filename in os.listdir("../2_python_part_2/files"):
+        with open(os.path.join("../2_python_part_2/files", filename), 'r') as f:  # open in readonly mode
+            text = text + (f'{f.read()} ')
+            mylist.append(str(text))
+    print(text)
+    with open(tmp_path, 'w', encoding='CP1252') as f:
+        fields = text.split(' ')
+        f.write(','.join(fields))
--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -4,3 +4,16 @@ To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+
+import sys
+import os
+
+sys.path.append('../2_python_part_2')
+import task_read_write
+
+def test_create_file(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "result.txt"
+    task_read_write.read_write(p)
+    assert (os.path.exists(p))
