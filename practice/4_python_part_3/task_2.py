5bd45a9a03a5fffc62e8f9b198e78299db28eeed
dincrash <dincrash@yandex.ru>
2022-02-08 18:32:18 +0300

    4 math 2

--- practice/4_python_part_3/task_2.py
+++ practice/4_python_part_3/task_2.py
@@ -13,9 +13,33 @@ import math
 
 
 def math_calculate(function: str, *args):
-    ...
+    if (function == 'log'):
+        return log(*args)
+    elif (function == 'ceil'):
+        return ceil(*args)
+    else:
+        return ("OperationNotFoundException")
 
 
+def log(*args):
+    return math.log(*args)
+
+
+def ceil(*args):
+    return math.ceil(*args)
+
+
+#math_calculate('log', 1024, 2)
+#math_calculate('log', 1024)
+#math_calculate('ceil', 10.7)
+#math_calculate('ceils', 10.7)
 """
 Write tests for math_calculate function
 """
+
+
+def test_log():
+    assert math_calculate('log', 1024, 2) == math.log(1024, 2)
+
+def test_ceil():
+    assert math_calculate('ceil', 10.7) == math.ceil(10.7)
