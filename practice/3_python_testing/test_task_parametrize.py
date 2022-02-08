9c9b1d49b132e3e66f4bca525b74655795f33ebf
dincrash <dincrash@yandex.ru>
2022-02-08 16:05:54 +0300

    3 fibonacci parametrize+ fix bug 6

--- practice/3_python_testing/test_task_parametrize.py
+++ practice/3_python_testing/test_task_parametrize.py
@@ -9,17 +9,28 @@ Task:
  1. Write a test with @pytest.mark.parametrize decorator.
  2. Find the buggy function and fix it.
 """
+import pytest
 
 
 def fibonacci_1(n):
     a, b = 0, 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         a, b = b, a + b
     return b
 
 
 def fibonacci_2(n):
     fibo = [0, 1]
-    for i in range(1, n+1):
-        fibo.append(fibo[i-1] + fibo[i-2])
+    for i in range(1, n + 1):
+        fibo.append(fibo[i] + fibo[i - 1])
     return fibo[n]
+
+
+@pytest.mark.parametrize("a, expected_result", [(6, 8), (30, 832040), (45, 1134903170)])
+def test_fibonacci_1(a, expected_result):
+    assert (expected_result == fibonacci_1(a))
+
+
+@pytest.mark.parametrize("a, expected_result", [(6, 8), (30, 832040), (45, 1134903170)])
+def test_fibonacci_2(a, expected_result):
+    assert (expected_result == fibonacci_2(a))
