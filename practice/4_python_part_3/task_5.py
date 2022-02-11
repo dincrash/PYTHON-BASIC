9a8eaf3b90680cf9eae6e64b68040c4d1fa80ae3
dincrash <dincrash@yandex.ru>
2022-02-11 15:57:01 +0300

    task 5

--- practice/4_python_part_3/task_5.py
+++ practice/4_python_part_3/task_5.py
@@ -2,11 +2,12 @@
 Write a function that makes a request to some url
 using urllib. Return status code and decoded response data in utf-8
 Examples:
-     >>> make_request('https://www.google.com')
+     #>>> make_request('https://www.google.com')
      200, 'response data'
 """
 import urllib.request
 from typing import Tuple
+from unittest.mock import Mock
 
 
 def make_request(url: str) -> Tuple[int, str]:
@@ -14,7 +15,7 @@ def make_request(url: str) -> Tuple[int, str]:
     return urlcode.getcode(), urlcode.read().decode('utf8')
 
 
-make_request("https://www.google.com")
+#make_request("https://www.google.com")
 """
 Write test for make_request function
 Use Mock for mocking request with urlopen https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock
@@ -27,3 +28,12 @@ Example:
     >>> m.method2()
     b'some text'
 """
+
+def test_make_request():
+    mr = make_request("https://www.google.com")
+    m = Mock()
+    m.make_request.return_value = 200
+    m.make_request()
+    assert(m.make_request())
+
+
