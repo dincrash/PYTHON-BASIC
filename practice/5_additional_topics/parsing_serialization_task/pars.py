dcfa78da8a352e16a4283aaa1231af7381af7970
dincrash <dincrash@yandex.ru>
2022-02-11 19:45:08 +0300

    block 5

--- practice/5_additional_topics/parsing_serialization_task/pars.py
+++ practice/5_additional_topics/parsing_serialization_task/pars.py
@@ -1,39 +1,4 @@
-import xml.etree.ElementTree as gfg
+import os
 
-
-def GenerateXML(fileName):
-    root = gfg.Element("Catalog")
-
-    m1 = gfg.Element("cities")
-    root.append(m1)
-
-    b1 = gfg.SubElement(m1, "brand")
-    b1.text = "Redmi"
-    b2 = gfg.SubElement(m1, "price")
-    b2.text = "6999"
-
-    m2 = gfg.Element("mobile")
-    root.append(m2)
-
-    c1 = gfg.SubElement(m2, "brand")
-    c1.text = "Samsung"
-    c2 = gfg.SubElement(m2, "price")
-    c2.text = "9999"
-
-    m3 = gfg.Element("mobile")
-    root.append(m3)
-
-    d1 = gfg.SubElement(m3, "brand")
-    d1.text = "RealMe"
-    d2 = gfg.SubElement(m3, "price")
-    d2.text = "11999"
-
-    tree = gfg.ElementTree(root)
-
-    with open(fileName, "wb") as files:
-        tree.write(files)
-
-
-# Driver Code
-if __name__ == "__main__":
-    GenerateXML("Catalog.xml")
+for i in os.scandir("../parsing_serialization_task/source_data"):
+    print(i.name)
--- practice/5_additional_topics/parsing_serialization_task/parsing_json.py
+++ practice/5_additional_topics/parsing_serialization_task/parsing_json.py
@@ -1,40 +1,17 @@
 import json
+import os
 import xml.etree.ElementTree as gfg
 
-json1_file = open('2021_09_25.json')
-json1_str = json1_file.read()
-json1_data = json.loads(json1_str)
 
-# print(json1_data['current'])
-# print(json1_data['hourly']['temp'])
-all_temp = []
-for i in json1_data['hourly']:
-    all_temp.append(i['temp'])
-print(f'{sum(all_temp) / len(all_temp):.2f}')
-print(f'{max(all_temp):.2f}')
-print(f'{min(all_temp):.2f}')
-all_wind = []
-for i in json1_data['hourly']:
-    all_wind.append(i['wind_speed'])
-print(f'{sum(all_wind) / len(all_wind):.2f}')
-print(f'{max(all_wind):.2f}')
-print(f'{min(all_wind):.2f}')
-
-
-def GenerateXML(fileName):
+def GenerateXML(fileName, mylist):
     root = gfg.Element("weather", country="Spain", date="2021-09-25")
     m2 = gfg.Element("summary")
     root.append(m2)
     m1 = gfg.Element("cities")
     root.append(m1)
-    a = f'{sum(all_temp) / len(all_temp):.2f}'
-    b = f'{sum(all_wind) / len(all_wind):.2f}'
-    c = f'{min(all_temp):.2f}'
-    d = f'{min(all_wind):.2f}'
-    e = f'{max(all_temp):.2f}'
-    f = f'{max(all_wind):.2f}'
-    b1 = gfg.SubElement(m1, "Barcelona", mean_temp=a, mean_wind_speed=b, min_temp=c, min_wind_speed=d, max_temp=e,
-                        max_wind_speed=f)
+    for i in mylist:
+        b1 = gfg.SubElement(m1, i[0], mean_temp=i[1], mean_wind_speed=i[2], min_temp=i[3], min_wind_speed=i[4], max_temp=i[5],
+                            max_wind_speed=i[6])
 
     tree = gfg.ElementTree(root)
 
@@ -42,5 +19,38 @@ def GenerateXML(fileName):
         tree.write(files)
 
 
+def read_files():
+    mylist = []
+    text = ""
+    for i in os.scandir("../parsing_serialization_task/source_data"):
+        # print(i.name)
+        path = "../parsing_serialization_task/source_data/" + i.name
+        path = path + "/2021_09_25.json"
+        name = i.name
+        json1_file = open(path)
+        json1_str = json1_file.read()
+        json1_data = json.loads(json1_str)
+        # print(json1_data)
+        # print(json1_data['current'])
+        # print(json1_data['hourly']['temp'])
+        all_temp = []
+        for i in json1_data['hourly']:
+            all_temp.append(i['temp'])
+
+        all_wind = []
+        for i in json1_data['hourly']:
+            all_wind.append(i['wind_speed'])
+
+        a = f'{sum(all_temp) / len(all_temp):.2f}'
+        b = f'{sum(all_wind) / len(all_wind):.2f}'
+        c = f'{min(all_temp):.2f}'
+        d = f'{min(all_wind):.2f}'
+        e = f'{max(all_temp):.2f}'
+        f = f'{max(all_wind):.2f}'
+        mylist.append([name, a, b, c, d, e, f])
+    print(mylist)
+    GenerateXML("Catalog.xml", mylist)
+
+
 if __name__ == "__main__":
-    GenerateXML("Catalog.xml")
+    read_files()
