21494e6e70a64cffbea6869f46cdac993f78831f
dincrash <dincrash@yandex.ru>
2022-02-11 19:02:25 +0300

    block 5

--- practice/5_additional_topics/parsing_serialization_task/parsing_json.py
+++ practice/5_additional_topics/parsing_serialization_task/parsing_json.py
@@ -1,7 +1,6 @@
 import json
 import xml.etree.ElementTree as gfg
 
-
 json1_file = open('2021_09_25.json')
 json1_str = json1_file.read()
 json1_data = json.loads(json1_str)
@@ -23,17 +22,25 @@ print(f'{min(all_wind):.2f}')
 
 
 def GenerateXML(fileName):
-    root = gfg.Element("Catalog")
+    root = gfg.Element("weather", country="Spain", date="2021-09-25")
+    m2 = gfg.Element("summary")
+    root.append(m2)
     m1 = gfg.Element("cities")
     root.append(m1)
-
-    b1 = gfg.SubElement(m1, "Barcelona")
-    b1.text = f'{sum(all_temp) / len(all_temp):.2f}'
+    a = f'{sum(all_temp) / len(all_temp):.2f}'
+    b = f'{sum(all_wind) / len(all_wind):.2f}'
+    c = f'{min(all_temp):.2f}'
+    d = f'{min(all_wind):.2f}'
+    e = f'{max(all_temp):.2f}'
+    f = f'{max(all_wind):.2f}'
+    b1 = gfg.SubElement(m1, "Barcelona", mean_temp=a, mean_wind_speed=b, min_temp=c, min_wind_speed=d, max_temp=e,
+                        max_wind_speed=f)
 
     tree = gfg.ElementTree(root)
 
     with open(fileName, "wb") as files:
         tree.write(files)
+
 
 if __name__ == "__main__":
     GenerateXML("Catalog.xml")
