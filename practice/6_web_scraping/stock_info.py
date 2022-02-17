13d21e2df6cfa1da3eb3df3ea5f5e91fe402f8c6
dincrash <dincrash@yandex.ru>
2022-02-17 15:15:25 +0300

    block 6 all

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -31,6 +31,7 @@ Links:
     - beautiful soup docs: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
     - lxml docs: https://lxml.de/
 """
+import re
 
 from bs4 import BeautifulSoup
 import requests
@@ -87,9 +88,119 @@ def youngestceo():
         t.add_row(
             [str(i['Name']), str(i['Code']), str(i['Country']), str(i['Employees']), str(i['CEO']), str(i['Age'])])
     print(t)
+
+
 def bestchange():
-    pass
+    url = 'https://finance.yahoo.com/most-active'
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
+    }
+    response = requests.get(url, headers=header)
+    soup = BeautifulSoup(response.content, 'lxml')
+    liststock = []
+    listname = []
+    for item in soup.select('.simpTblRow'):
+        liststock.append(item.select('[aria-label=Symbol]')[0].get_text())
+        listname.append(item.select('[aria-label=Name]')[0].get_text())
+
+    b = 0
+    output = {"Code": [], "Name": [], "52-Week": [], "Total Cash": []}
+    out = []
+    # Name, Code, 52-Week Change, Total Cash
+    while b <= (len(liststock) - 1):
+        output["Code"].append(liststock[b])
+        output["Name"].append(listname[b])
+        url = 'https://finance.yahoo.com/quote/' + liststock[b] + '/key-statistics?p=' + liststock[b]
+        response = requests.get(url, headers=header)
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        output["52-Week"].append(
+            re.sub('[%]', '', html_soup.select('[class="Fw(500) Ta(end) Pstart(10px) Miw(60px)"]')[10].get_text()))
+        try:
+            output["Total Cash"].append(
+                html_soup.select('[class="Fw(500) Ta(end) Pstart(10px) Miw(60px)"]')[52].get_text())
+        except IndexError:
+            pass
+        out.append(output.copy())
+        output = {"Code": [], "Name": [], "52-Week": [], "Total Cash": []}
+        b = b + 1
+    newout = sorted(out, key=lambda d: d['52-Week'])
+    i = 0
+    while i < (len(out) - 5):
+        del newout[0]
+        i = i + 1
+    print("==================================== best 52-week change ===================================")
+    t = PrettyTable(['Name', 'Code', '52-Week', 'Total Cash'])
+    for i in newout:
+        t.add_row(
+            [str(i['Name']), str(i['Code']), str(i['52-Week']), str(i['Total Cash'])])
+    print(t)
+
+
+def blackrock():
+    url = 'https://finance.yahoo.com/most-active'
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
+    }
+    response = requests.get(url, headers=header)
+    soup = BeautifulSoup(response.content, 'lxml')
+    liststock = []
+    listname = []
+    for item in soup.select('.simpTblRow'):
+        liststock.append(item.select('[aria-label=Symbol]')[0].get_text())
+        listname.append(item.select('[aria-label=Name]')[0].get_text())
+    b = 0
+    out = []
+    output = {"Name": [], "Code": [], "Shares": [], "Date": [], "%": [], "Value": []}
+    while b <= (len(liststock) - 1):
+        output["Code"].append(liststock[b])
+        output["Name"].append(listname[b])
+
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
+        }
+        url = 'https://finance.yahoo.com/quote/' + liststock[b] + '/holders?p=' + liststock[b]
+        response = requests.get(url, headers=header)
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+
+        i = 0
+        a = ''
+        try:
+            while a != "Blackrock Inc.":
+                a = html_soup.select('[class="Ta(start) Pend(10px)"]')[i].get_text()
+                i = i + 1
+        except IndexError:
+            pass
+        procent = (re.sub('[%]', '', html_soup.select('[class="Ta(start) Pend(10px)"]')[
+            i - 1].nextSibling.nextSibling.nextSibling.get_text()))
+        shares = (html_soup.select('[class="Ta(start) Pend(10px)"]')[i - 1].nextSibling.get_text())
+        date = (html_soup.select('[class="Ta(start) Pend(10px)"]')[i - 1].nextSibling.nextSibling.get_text())
+        value = (html_soup.select('[class="Ta(start) Pend(10px)"]')[
+                     i - 1].nextSibling.nextSibling.nextSibling.nextSibling.get_text())
+
+        # Name, Code, Shares, Date Reported, % Out, Value.
+        output["%"].append(procent)
+        output["Shares"].append(shares)
+        output["Date"].append(date)
+        output["Value"].append(value)
+        out.append(output)
+        output = {"Name": [], "Code": [], "Shares": [], "Date": [], "%": [], "Value": []}
+        b = b + 1
+    newout = sorted(out, key=lambda d: d['%'])
+    i = 0
+    while i < (len(out) - 10):
+        del newout[0]
+        i = i + 1
+    print("==================================== largest holding blackrock % ===================================")
+    t = PrettyTable(['Name', 'Code', 'Shares', 'Date', '%', 'Value'])
+    for i in newout:
+        t.add_row(
+            [str(i['Name']), str(i['Code']), str(i['Value']), str(i['Date']), str(i['%']), str(i['Value'])])
+    print(t)
+
+
 # 5 stocks with most youngest CEOs
-# youngestceo()
+youngestceo()
 # 10 stocks with best 52-Week Change
 bestchange()
+# 10 largest holds of Blackrock Inc.
+blackrock()
--- practice/6_web_scraping/test.py
+++ practice/6_web_scraping/test.py
@@ -1,19 +1,2 @@
-from prettytable import PrettyTable
-output = {"Code": [], "Name": [], "Country": [], "Employees": [], "CEO": [], "Age": []}
-out = []
-output["Code"].append("liststock[b]")
-output["Name"].append("listname[b]")
-
-out.append(output.copy())
-output = {"Code": [], "Name": [], "Country": [], "Employees": [], "CEO": [], "Age": []}
-output["Code"].append("liststock[b]")
-output["Name"].append("listname[b]")
-out.append(output.copy())
-newout = out
-
-    #print('{:<10s}{:>4s}'.format(" | ".join(i['Address'])),(", ".join(i['Name'])))))
-
-t = PrettyTable(['Name', 'Code', 'Country', 'Employees', 'CEO', 'Age'])
-for i in newout:
-    t.add_row([i['Name'], i['Code'], i['Country'], i['Employees'], i['CEO'], i['Age']])
-print(t)
+import re
+print(re.sub('[%]','', '10%'))
